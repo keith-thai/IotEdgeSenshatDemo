1f46c0dc47eb298b9b6315b645132c825fa9b4be
Ted Malone <temalo@microsoft.com>
2019-02-23 08:56:46 -0700

    Modifications to message format and routing

--- modules/SenseHatSensors/main.py
+++ modules/SenseHatSensors/main.py
@@ -16,7 +16,7 @@ from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubE
 # messageTimeout - the maximum time in milliseconds until a message times out.
 # The timeout period starts at IoTHubModuleClient.send_event_async.
 # By default, messages do not expire.
-MESSAGE_TIMEOUT = 10000
+MESSAGE_TIMEOUT = 1000000
 
 # global counters
 RECEIVE_CALLBACKS = 0
@@ -25,6 +25,7 @@ SEND_SENSEHAT_CALLBACKS = 0
 
 # Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
 PROTOCOL = IoTHubTransportProvider.MQTT
+VERBOSE = False
 
 # read sense hat and send 
 def read_and_send_measurements_from_sensehat(hubManager):
@@ -44,21 +45,22 @@ def read_and_send_measurements_from_sensehat(hubManager):
     MSG_TXT = "{\"temperatureF\": %.2f,\"temperatureC\": %.2f,\"pressureTemperature\": %.2f,\"humidity\": %.2f,\"pressure\": %.2f,\"timeCreated\": \"%s\"}"    
     msg_txt_formatted = MSG_TXT % (temperature, temperature_h, temperature_p, humidity, pressure, timeCreated)    
     message = IoTHubMessage(msg_txt_formatted)    
-    hubManager.forward_event_to_output("output2", message, 0)
     SEND_SENSEHAT_CALLBACKS += 1
-    print ("The Current Temperature is: ", temperature, "F")
-    print ( "    Total sensehat messages sent: %d" % SEND_SENSEHAT_CALLBACKS )
+    print ( "The Current Temperature is: ", temperature, "F")
+    print ( "Sending message ", SEND_SENSEHAT_CALLBACKS, " to upstream")
+    print (message)
+    hubManager.forward_event_to_output("output1", message, 0)
     
 
 # Callback received when the message that we're forwarding is processed.
 def send_confirmation_callback(message, result, user_context):
     global SEND_CALLBACKS
-    print ( "Confirmation[%d] received for message with result = %s" % (user_context, result) )
+    print ( "Confirmation[",user_context,"] received for message with result =" , result) 
     map_properties = message.properties()
     key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
+    print ( "    Properties: ", key_value_pair )
     SEND_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
+    print ( "    Total calls confirmed: ", SEND_CALLBACKS )
 
 
 # receive_message_callback is invoked when an incoming message arrives on the specified 
@@ -68,13 +70,14 @@ def receive_message_callback(message, hubManager):
     global RECEIVE_CALLBACKS
     message_buffer = message.get_bytearray()
     size = len(message_buffer)
-    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode('utf-8'), size) )
+    print ( "Data: <<<",message_buffer[:size].decode('utf-8'),">>> & Size=",size )
     map_properties = message.properties()
     key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
+    print ( "    Properties: ", key_value_pair )
     RECEIVE_CALLBACKS += 1
-    print ( "    Total calls received: %d" % RECEIVE_CALLBACKS )
-    hubManager.forward_event_to_output("output1", message, 0)
+    print ( "    Total calls received: ", RECEIVE_CALLBACKS )
+    print ("Sending message to output2")
+    hubManager.send_event_to_output("output2", message, 0)
     return IoTHubMessageDispositionResult.ACCEPTED
 
 
@@ -82,17 +85,27 @@ class HubManager(object):
 
     def __init__(
             self,
-            protocol=IoTHubTransportProvider.MQTT):
+            messageTimeout,
+            protocol,
+            verbose):
+        '''
+        Communicate with the Edge Hub
+
+        :param int messageTimeout: the maximum time in milliseconds until a message times out. The timeout period starts at IoTHubClient.send_event_async. By default, messages do not expire.
+        :param IoTHubTransportProvider protocol: Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
+        :param bool verbose: set to true to get detailed logs on messages
+        '''
+        self.messageTimeout = messageTimeout
         self.client_protocol = protocol
         self.client = IoTHubModuleClient()
         self.client.create_from_environment(protocol)
+        self.client.set_option("messageTimeout", self.messageTimeout)
+        self.client.set_option("product_info","SenseHatSensors Module")
+        if verbose:
+            self.client.set_option("logtrace", 1)#enables MQTT logging
 
-        # set the time until a message times out
-        self.client.set_option("messageTimeout", MESSAGE_TIMEOUT)
-        
-        # sets the callback when a message arrives on "input1" queue.  Messages sent to 
-        # other inputs or to the default will be silently discarded.
-        self.client.set_message_callback("input1", receive_message_callback, self)
+    def send_event_to_output(self, outputQueueName, event, send_context):
+        self.client.send_event_async(outputQueueName, event, send_confirmation_callback, send_context)
 
     # Forwards the message received onto the next stage in the process.
     def forward_event_to_output(self, outputQueueName, event, send_context):
@@ -101,22 +114,22 @@ class HubManager(object):
 
 def main(protocol):
     try:
-        print ( "\nPython %s\n" % sys.version )
-        print ( "IoT Hub Client for Python" )
+        print ( "\nPython \n", sys.version )
+        print ( "IoT Edge Client for Python" )
 
-        hub_manager = HubManager(protocol)
+        hub_manager = HubManager(MESSAGE_TIMEOUT,PROTOCOL,VERBOSE)
 
-        print ( "Starting the IoT Hub Python module using protocol %s..." % hub_manager.client_protocol )
-        print ( "The device is now waiting for messages and will continue indefinitely.  Press Ctrl-C to exit. ")
+        print ( "Starting the Edge Python module using protocol ",hub_manager.client_protocol )
+        print ( "The device is now waiting for messages ")
 
         while True:
             #Start sending environmental values
             read_and_send_measurements_from_sensehat(hub_manager)
-            #wait for a minute
-            time.sleep(60)
+            #wait for 30 seconds
+            time.sleep(30)
 
     except IoTHubError as iothub_error:
-        print ( "Unexpected error %s from IoTHub" % iothub_error )
+        print ( "Unexpected error from IoTHub:",iothub_error )
         return
     except KeyboardInterrupt:
         print ( "IoTHubModuleClient sample stopped" )
